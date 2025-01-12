7002047089ada7b25cdc7726ee1e9e3f203b5f81
manu <manu@Ubuntu-Metodi.myguest.virtualbox.org>
2025-01-12 21:37:17 +0100

    inserite altre 2 versioni di intercalare

--- es10-01-25/inter.c
+++ es10-01-25/inter.c
@@ -9,11 +9,46 @@ Si scriva una programma che legge 2  vettori, e costruisce un terzo vettore, che
 
 #include <stdio.h>
 
-void intercalare(int v1[], int v2[], int n, int v3[]){
+#define false 0
+#define true 1
+
+typedef int boolean;
+
+void intercalare_v1(int v1[], int v2[], int n, int v3[]){
 
     for(int i = 0; i<n; i++){
         v3[2*i] = v1[i];
         v3[2*i+1] = v2[i];
+    }
+}
+
+void intercalare_v2(int v1[], int v2[], int n, int v3[]){
+
+    int j=0;
+    for(int i = 0; i<2*n; i++){
+        if(j==0){
+            v3[i] = v1[i/2];
+            j++;
+        }else{
+            v3[i] = v2[(i-1)/2];
+            j=0;
+        }
+        
+    }
+}
+
+void intercalare_v3 (int v1[], int v2[], int n, int v3[]){
+
+    boolean el_v1=true;
+    for(int i = 0; i<2*n; i++){
+        if(el_v1){
+            v3[i] = v1[i/2];
+            el_v1=false;
+        }else{
+            v3[i] = v2[(i-1)/2];
+            el_v1=true;
+        }
+        
     }
 }
 
@@ -28,7 +63,7 @@ int main(){
 
     int v3[2*n];
 
-    intercalare(v1, v2, n, v3);
+    intercalare_v3(v1, v2, n, v3);
 
     printf("Vettore risultato: ");
     for(int i=0; i<2*n; i++){
